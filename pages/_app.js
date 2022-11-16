8b4002f5eeb84aacbce78d140b1484a30b376a77
mukesh <mukeshkumar42000000@gmail.com>
2022-11-16 11:06:03 +0530

    link

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+
 import '../styles/globals.css'
 import '../styles/media.css'
 import '../styles/medium.css'
@@ -9,12 +9,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
-          </Head>
+      
      <Component {...pageProps} />
 
   </>
